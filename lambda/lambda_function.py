7600d00130176f93dacb995a9afa3f14ac843de0
JDTweddle <jdtweddle@gmail.com>
2024-09-20 13:47:14 +0100

    update for text in lambda function

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello, from CICD Lambda!')  # This will be returned as the response body.
+        'body': json.dumps('Hello, from CICD actions Lambda!')  # This will be returned as the response body.
     }
